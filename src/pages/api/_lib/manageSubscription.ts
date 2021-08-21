823cbc699736833f10b8d4768a0b721ddec4023b
carloscenci <carlosc.estevam@gmail.com>
2021-08-21 15:06:44 -0300

    Finish webhooks with update and delete subscription

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -6,10 +6,11 @@ import { stripe } from '../../../services/stripe';
 export async function saveSubscription(
     subscriptionId: string,
     customerId: string,
+    createAction = false
 ) { 
     const userRef = await fauna.query(
         q.Select(
-            'ref',
+            "ref",
             q.Get(
                 q.Match(
                     q.Index('user_by_stripe_costumer_id'),
@@ -25,13 +26,34 @@ export async function saveSubscription(
         id: subscription.id,
         userId: userRef,
         status: subscription.status,
-        price_id: subscription.items.data[0].price.id,
+        price_id: subscription.items.data[0].price.id,  
     }
 
-    await fauna.query(
-        q.Create(
-            q.Collection('subscriptions'),
-            { data: subscriptionData }
+    if(createAction) {
+        await fauna.query(
+            q.Create(
+                q.Collection('subscriptions'),
+                { data: subscriptionData }
+            )
         )
-    )
+    } else {
+        const subscriptionRef = await fauna.query(
+          q.Select(
+            "ref",
+            q.Get(
+              q.Match(
+                q.Index('subscription_by_id'),
+                subscriptionId
+              ),
+            )
+          )
+        );
+        await fauna.query(
+          q.Replace(
+            subscriptionRef,
+            {data: subscriptionData}
+          )
+        )
+      }
+      
 }
