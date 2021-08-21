de21e8d0d9597d77b99afc8dea03b1454d5f00da
carloscenci <carlosc.estevam@gmail.com>
2021-08-21 13:30:28 -0300

    Create webhooks and create subscriptions

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -7,9 +7,7 @@ export const stripe = new Stripe(
         apiVersion: '2020-08-27',
         appInfo: {
             name: "IGNews",
-            version
+            version : version
         },
     }
 )
-
-// 
