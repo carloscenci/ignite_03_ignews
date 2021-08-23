1e533151e5752a6a3bfe97284518b05ba8d0f642
carlosc <carloscenci@grupobrisanet.com.br>
2021-08-23 09:13:28 -0300

    Create a validation access in page post and creave preview post

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,12 +1,12 @@
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import { getSession } from "next-auth/client"
 import Head from "next/head"
 import { RichText } from "prismic-dom"
-import { getPrismicClient } from "../../services/prismic"
+import { getPrismicClient } from "../../../services/prismic"
 
-import styles from './post.module.scss'
+import styles from '../post.module.scss'
 
-interface PostProps {
+interface PostPreviewProps {
     post: {
         slug: string;
         title: string;
@@ -15,7 +15,7 @@ interface PostProps {
     }
 }
 
-export default function Post({post}: PostProps) {
+export default function PostPreview({post}: PostPreviewProps) {
     return (
         <>
             <Head>
@@ -33,20 +33,17 @@ export default function Post({post}: PostProps) {
     )
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-    const session = await getSession({req})
+export const getStaticPaths = () => {
+    return {
+        paths: [],
+        fallback: 'blocking'
+    }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
     const { slug } = params;
 
-    if(!session.activeSubscription) {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false
-            }
-        }
-    }
-
-    const prismic = getPrismicClient(req)
+    const prismic = getPrismicClient()
 
     const response = await prismic.getByUID('post', String(slug), {})
 
