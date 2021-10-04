5515d44bc1a8502495c17bad2f57bc699726716b
Carlos Cenci <carlosc.estevam@gmail.com>
2021-10-04 17:24:41 -0300

    Finish all the tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,22 @@
 module.exports = {
     testIgnorePatterns: ["/node_modules/", "/.next/"],
+    setupFilesAfterEnv: [
+        "<rootDir>/src/tests/setupTests.ts"  
+    ],
     transform: {
-        "^.+\\.(js|jsx|ts|tsx)$" : "<rootDir>"
+        "^.+\\.(js|jsx|ts|tsx)$" : "<rootDir>/node_modules/babel-jest"
     },
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        "\\.(scss|css|sass)$" : "identity-obj-proxy"
+    },
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "src/**/*.tsx",
+        "!src/**/*.spec.tsx",
+        "!src/**/_app.tsx",
+        "!src/**/_document.tsx",
+        // "!**/node_modules/**",
+    ],
+    coverageReporters: ["lcov", "json"]
 }
